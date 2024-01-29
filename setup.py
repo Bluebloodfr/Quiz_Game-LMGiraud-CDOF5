2d41ab9c70eab80c0789f8d20ac467100d267346
Bluebloodfr <louismelchiorgiraud@gmail.com>
2024-01-29 17:31:15 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Quiz Game',
+ version='1.0',
+ author='Louis-Melchior Giraud',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
